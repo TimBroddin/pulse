78a5878df2d97a2c660fa51324fecb68ddbddac8
Tim Broddin <tim@wannabes.be>
2020-06-15 11:59:26 +0200

    Update PULSE.py

--- PULSE.py
+++ PULSE.py
@@ -20,7 +20,7 @@ class PULSE(torch.nn.Module):
         cache_dir = Path(cache_dir)
         cache_dir.mkdir(parents=True, exist_ok = True)
         if self.verbose: print("Loading Synthesis Network")
-        with open_url("https://drive.google.com/uc?id=1TCViX1YpQyRsklTVYEJwdbmK91vklCo8", cache_dir=cache_dir, verbose=verbose) as f:
+        with open_url("https://drive.google.com/uc?id=1en2t4bObHdT1RqDi5QBT34ETQGqGBDYn", cache_dir=cache_dir, verbose=verbose) as f:
             self.synthesis.load_state_dict(torch.load(f))
 
         for param in self.synthesis.parameters():
@@ -33,8 +33,7 @@ class PULSE(torch.nn.Module):
         else:
             if self.verbose: print("\tLoading Mapping Network")
             mapping = G_mapping().cuda()
-
-            with open_url("https://drive.google.com/uc?id=14R6iHGf5iuVx3DMNsACAl7eBr7Vdpd0k", cache_dir=cache_dir, verbose=verbose) as f:
+            with open_url("https://drive.google.com/uc?id=1qFZNa2BvTMduy2tMLtuDzhP5EPIkzXDz", cache_dir=cache_dir, verbose=verbose) as f:
                     mapping.load_state_dict(torch.load(f))
 
             if self.verbose: print("\tRunning Mapping Network")
